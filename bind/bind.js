3cf522dc000e671976f6fa1bbbc0acbeb69c1982
段鸣鑫 <dmx@ishendi.com>
2020-09-22 17:55:34 +0800

    Add Function.prototype.__bind() .

---
+++ bind/bind.js
@@ -0,0 +1,54 @@
+/**
+ * 1. bind() 方法创建一个新的函数
+ * 2. 在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数
+ * 3. new 情况下忽略第一个参数
+ * 4. 其余参数将作为新函数的参数，供调用时使用
+ * @param {object} ctx
+ * @param  {...any} args
+ * @returns {function} 返回一个原函数的拷贝，并拥有指定 this 和初始参数
+ */
+Function.prototype.__bind = function (ctx, ...args) {
+    // 判断 this 是否为 function 类型
+    if (typeof this !== 'function') throw new TypeError('Error');
+
+    // 保存当前 this
+    const __this = this;
+
+    return function F() {
+        // new 绑定
+        if (this instanceof F) return new __this(...args, ...arguments);
+
+        // 直接调用时绑定 this
+        return __this.apply(ctx, [...args, ...arguments]);
+    };
+};
+
+// ------------------------------ 测试 ------------------------------
+
+function print() {
+    console.log(this.name, ...arguments);
+}
+
+const obj = {
+    name: 'mxin',
+};
+
+// Function.prototype.__bind
+console.log('Function.prototype.__bind');
+
+// 直接调用，返回原函数拷贝，this 指向 obj
+const F = print.__bind(obj, 26);
+F(178); // mxin, 26, 178
+
+// new 情况
+const _obj = new F(145); // undefined, 26, 145
+console.log(_obj); // print {}
+
+// Function.prototype.bind
+console.log('Function.prototype.bind');
+
+const Fn = print.bind(obj, 26);
+Fn(178); // mxin, 26, 178
+
+const __obj2 = new Fn(145); // undefined, 26, 145
+console.log(__obj2); // print {}
